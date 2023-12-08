16ceb54fd9d52c6b5394ea5a04e929052c98c8d4
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2023-12-08 20:11:38 +0000

    Figure out nodes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,37 @@
 function removeMenuItems(x) {
-  if (mediaQueryListObject.matches) { // If media query matches
-    document.body.style.backgroundColor = "yellow";
+
+  if (mediaQueryListObject.matches) {
+    document.getElementById("logo").remove();
+    document.getElementById("about").remove();
+    document.getElementById("services").remove();
+    document.getElementById("contact-us").remove();
+    
+    
+    // const nodes = document.getElementsByClassName("here")[0].childNodes;
+  
+    // for (let i = 0; i < nodes.length; i++) {
+    //   console.log(nodes[i].nodeType);
+    // }
+    
   } else {
-   document.body.style.backgroundColor = "pink";
+
+    const nodes = document.getElementsByClassName("here")[0].childNodes;
+
+    var elemNodeCount = 0;
+    for ( let i = 0; i < nodes.length; i++ ) {
+      // console.log(nodes[i].nodeType);
+      if ( nodes[i].nodeType == 1 ) {
+        elemNodeCount++;
+      }
+    }
+
+    if (elemNodeCount != 4 ) {
+      // Append child nodes
+      console.log("element nodes all gone")
+    } else {
+      console.log("4 element nodes remain.")
+    }
+  
   }
 }
 
@@ -16,3 +45,10 @@ removeMenuItems(mediaQueryListObject);
 mediaQueryListObject.addEventListener("change", function() {
     removeMenuItems(mediaQueryListObject);
 });
+
+var logo = document.getElementById("logo");
+var about = document.getElementById("about");
+var services = document.getElementById("services");
+var contactUs = document.getElementById("contact-us");
+
+
