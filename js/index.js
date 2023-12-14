fd9e61450a06f71018251dc9c54e993379d86251
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2023-12-14 19:03:54 +0000

    Do some more work on navbar.

--- js/index.js
+++ js/index.js
@@ -1,39 +1,113 @@
 function removeMenuItems(x) {
 
   if (mediaQueryListObject.matches) {
+
+    // Remove element nodes first
     document.getElementById("logo").remove();
     document.getElementById("about").remove();
     document.getElementById("services").remove();
     document.getElementById("contact-us").remove();
-    
-    
-    // const nodes = document.getElementsByClassName("here")[0].childNodes;
-  
-    // for (let i = 0; i < nodes.length; i++) {
-    //   console.log(nodes[i].nodeType);
-    // }
-    
+
+    // Append element node
+    const menuElem = document.createElement('img');
+    menuElem.setAttribute("style", "width:22px;height:16px;margin:25px;");
+    menuElem.setAttribute('src', "images/hamburger.png");
+    menuElem.setAttribute("id", "hamburger");
+    menuElem.addEventListener("click", dropDownMenu);
+
+    // Modify attributs for left-container
+    const lcNode = document.getElementsByClassName("left-container")[0];
+    lcNode.setAttribute("style", "width:auto;");
+    lcNode.appendChild(menuElem);
+
   } else {
 
-    const nodes = document.getElementsByClassName("here")[0].childNodes;
+    const nodes = document.getElementsByClassName("left-container")[0].childNodes;
 
     var elemNodeCount = 0;
-    for ( let i = 0; i < nodes.length; i++ ) {
-      // console.log(nodes[i].nodeType);
-      if ( nodes[i].nodeType == 1 ) {
+    for (let i = 0; i < nodes.length; i++) {
+      if (nodes[i].nodeType == 1) {
         elemNodeCount++;
       }
     }
 
-    if (elemNodeCount != 4 ) {
-      // Append child nodes
-      console.log("element nodes all gone")
-    } else {
-      console.log("4 element nodes remain.")
+    // Check if only element node is hamburger
+    if (elemNodeCount == 1) {
+      // Remove hamburger
+      document.getElementById("hamburger").remove();
+
+      // Modify attributs for left-container
+      const lcNode = document.getElementsByClassName("left-container")[0];
+      lcNode.setAttribute("style", "width:340px;");
+
+      // Append child nodes back in
+      document.getElementsByClassName("left-container")[0].appendChild(logo);
+      document.getElementsByClassName("left-container")[0].appendChild(about);
+      document.getElementsByClassName("left-container")[0].appendChild(services);
+      document.getElementsByClassName("left-container")[0].appendChild(contactUs);
+
+    } else if (elemNodeCount == 4) {
+
+      // Remove hamburger
+      document.getElementById("hamburger").remove();
+
+      // Modify attributs for left-container
+      const lcNode = document.getElementsByClassName("left-container")[0];
+      lcNode.setAttribute("style", "width:340px;");
+      
+
+
+
+
+
     }
-  
+
   }
 }
+
+function dropDownMenu() {
+
+  if (menuIsClosed) {
+
+    //
+    // Menu is closed so open it
+    //
+
+    // setup flex box
+    const lcElem = document.getElementsByClassName("left-container")[0];
+    lcElem.setAttribute("style", "flex-direction:column;width:auto;height:200px;justify-content:space-around;align-items:baseline;position:relative;top:108px;");
+
+    // Append child nodes back in
+    document.getElementsByClassName("left-container")[0].appendChild(about);
+    document.getElementsByClassName("left-container")[0].appendChild(services);
+    document.getElementsByClassName("left-container")[0].appendChild(contactUs);
+
+    // Flip menu control
+    menuIsClosed = false;
+
+  } else {
+
+    //
+    // Menu is open so close it
+    //
+
+    // Remove child nodes
+    document.getElementById("about").remove();
+    document.getElementById("services").remove();
+    document.getElementById("contact-us").remove();
+
+    // setup flex box
+    const lcElem = document.getElementsByClassName("left-container")[0];
+    lcElem.setAttribute("style", "flex-direction:row;width:auto;justify-content:flex-start;align-items:center;position:relative;top:50%;transform:translateY(-50%);");
+
+    // Flip menu control
+    menuIsClosed = true;
+
+  }
+}
+
+// Create menu control variable
+var menuIsClosed = true;
 
 // Create a MediaQueryList object
 var mediaQueryListObject = window.matchMedia("(max-width: 750px)")
@@ -42,13 +116,11 @@ var mediaQueryListObject = window.matchMedia("(max-width: 750px)")
 removeMenuItems(mediaQueryListObject);
 
 // Attach listener function on state changes
-mediaQueryListObject.addEventListener("change", function() {
-    removeMenuItems(mediaQueryListObject);
+mediaQueryListObject.addEventListener("change", function () {
+  removeMenuItems(mediaQueryListObject);
 });
 
 var logo = document.getElementById("logo");
 var about = document.getElementById("about");
 var services = document.getElementById("services");
 var contactUs = document.getElementById("contact-us");
-
-
